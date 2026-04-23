6ac0516c23ba3eb8cce75efb598dced633356e39
Patrick <pblood66@gmail.com>
2026-04-22 21:21:45 -0600

    first commit

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+typedef struct
+{
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer *new_input_buffer()
+{
+    InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("~ CQLite ~\n");
+
+    return 0;
+}
